4bf8f5c393ddaaad267cecd03bcbcce4bdc47fc6
Andrew Keiser <webdev410@gmail.com>
2023-11-04 19:43:43 -0400

    no comment

--- server.js
+++ server.js
@@ -13,35 +13,28 @@ app.get('/api/get-data', async (req, res) => {
 	try {
 		await init();
 
-		// After init, zip CSV files in the output directory
 		const output = fs.createWriteStream('output.zip');
 		const archive = archiver('zip', {
-			zlib: { level: 9 }, // Compression level
+			zlib: { level: 9 },
 		});
 
-		// Listen for all archive data to be written
 		output.on('close', function () {
 			console.log(`Archive wrote ${archive.pointer()} bytes`);
-			// Send the file to the client for download
 			res.download(path.resolve('output.zip'));
 		});
 
-		// Catch errors with the archiving process
 		archive.on('error', function (err) {
 			throw err;
 		});
 
-		// Pipe archive data to the file
 		archive.pipe(output);
 
-		// Append files from a directory
 		fs.readdirSync('./output').forEach((file) => {
 			if (path.extname(file) === '.csv') {
 				archive.file(path.join('./output', file), { name: file });
 			}
 		});
 
-		// Finalize the archive (this will append files to the zip file)
 		archive.finalize();
 	} catch (error) {
 		return res.status(500).json({ success: false, error });
